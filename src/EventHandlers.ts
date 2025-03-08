188826ea8394aaaec92623aa16ecd4af7b627c51
gwilldan <sayhi2godswill@gmail.com>
2025-03-08 20:07:00 +0100

    edited for the contract changes

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -1,53 +1,105 @@
-/*
- * Please refer to https://docs.envio.dev for a thorough guide on all Envio indexer features
- */
 import {
-  Monfundme,
-  Monfundme_CampaignClosed,
-  Monfundme_CampaignCompleted,
-  Monfundme_CampaignCreated,
-  Monfundme_DonationReceived,
+  Campaign,
+  Campaign_CampaignClosed,
+  Campaign_CampaignCompleted,
+  Campaign_DonationReceived,
+  Campaign_FeePaid,
+  Campaign_StatusUpdated,
+  Campaign_TargetReached,
+  Campaign_WithdrawalMade,
+  Factory,
+  Factory_CampaignCreated,
 } from "generated";
 
-Monfundme.CampaignClosed.handler(async ({ event, context }) => {
-  const entity: Monfundme_CampaignClosed = {
+Factory.CampaignCreated.handler(async ({ event, context }) => {
+  const entity: Factory_CampaignCreated = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-    campaignId: event.params.campaignId,
+    campaignAddress: event.params.campaignAddress,
+    owner: event.params.owner,
+    event_id: event.params.id,
   };
 
-  context.Monfundme_CampaignClosed.set(entity);
+  context.Factory_CampaignCreated.set(entity);
 });
 
-Monfundme.CampaignCompleted.handler(async ({ event, context }) => {
-  const entity: Monfundme_CampaignCompleted = {
+Factory.CampaignCreated.contractRegister(async ({ event, context }) => {
+  context.addCampaign(event.params.campaignAddress)
+}, { preRegisterDynamicContracts: true })
+
+Campaign.CampaignClosed.handler(async ({ event, context }) => {
+  const entity: Campaign_CampaignClosed = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-    event_id: event.params.id,
+    campaignAddress: event.srcAddress,
+    timestamp: event.params.timestamp,
+  };
+
+  context.Campaign_CampaignClosed.set(entity);
+});
+
+Campaign.CampaignCompleted.handler(async ({ event, context }) => {
+  const entity: Campaign_CampaignCompleted = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    campaignAddress: event.srcAddress,
     amountCollected: event.params.amountCollected,
   };
 
-  context.Monfundme_CampaignCompleted.set(entity);
+  context.Campaign_CampaignCompleted.set(entity);
 });
 
-Monfundme.CampaignCreated.handler(async ({ event, context }) => {
-  const entity: Monfundme_CampaignCreated = {
+Campaign.DonationReceived.handler(async ({ event, context }) => {
+  const entity: Campaign_DonationReceived = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-    campaignId: event.params.campaignId,
-    owner: event.params.owner,
-    title: event.params.title,
-    target: event.params.target,
-    deadline: event.params.deadline,
-  };
-
-  context.Monfundme_CampaignCreated.set(entity);
-});
-
-Monfundme.DonationReceived.handler(async ({ event, context }) => {
-  const entity: Monfundme_DonationReceived = {
-    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-    campaignId: event.params.campaignId,
+    campaignAddress: event.srcAddress,
     donator: event.params.donator,
     amount: event.params.amount,
+    transactionHash: event.transaction.hash,
   };
 
-  context.Monfundme_DonationReceived.set(entity);
+  context.Campaign_DonationReceived.set(entity);
 });
+
+Campaign.FeePaid.handler(async ({ event, context }) => {
+  const entity: Campaign_FeePaid = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    campaignAddress: event.srcAddress,
+    to: event.params.to,
+    amount: event.params.amount,
+    transactionHash: event.transaction.hash,
+  };
+
+  context.Campaign_FeePaid.set(entity);
+});
+
+Campaign.StatusUpdated.handler(async ({ event, context }) => {
+  const entity: Campaign_StatusUpdated = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    campaignAddress: event.srcAddress,
+    newStatus: event.params.newStatus,
+  };
+
+  context.Campaign_StatusUpdated.set(entity);
+});
+
+Campaign.TargetReached.handler(async ({ event, context }) => {
+  const entity: Campaign_TargetReached = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    campaignAddress: event.srcAddress,
+    timestamp: event.params.timestamp,
+  };
+
+  context.Campaign_TargetReached.set(entity);
+});
+
+Campaign.WithdrawalMade.handler(async ({ event, context }) => {
+  const entity: Campaign_WithdrawalMade = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    campaignAddress: event.srcAddress,
+    to: event.params.to,
+    amount: event.params.amount,
+    transactionHash: event.transaction.hash,
+  };
+
+  context.Campaign_WithdrawalMade.set(entity);
+});
+
+
